7f2a89abb5c2dc1f2197291fabb30954506d67b6
Iamneo <techadmin@iamneo.ai>
2023-09-30 05:20:05 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import QuoteGenerator from './components/QuoteGenerator';
+import quotes from './quotes'; // Import the quotes data source
 
 function App() {
+  // State to hold the currently displayed quote and author
+  const [currentQuote, setCurrentQuote] = useState({
+    text: "Loading...",
+    author: "",
+  });
+
+  // Function to display a random quote
+  const displayRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+    setCurrentQuote(randomQuote);
+  };
+
+  // Load a random quote when the component first mounts
+  useEffect(() => {
+    displayRandomQuote();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Random Quote Generator</h1>
       </header>
+      <main>
+        <QuoteGenerator
+          currentQuote={currentQuote}
+          displayRandomQuote={displayRandomQuote}
+        />
+      </main>
     </div>
   );
 }
